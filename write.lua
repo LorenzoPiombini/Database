0e4f1a6c646b37136cee50b18d9517071d586563
Lorenzo Piombini <piombinilorenzo1987@gmail.com>
2025-12-20 11:22:53 -0500

    docs: write to a file via Lua script

---
+++ write.lua
@@ -0,0 +1,25 @@
+--- Lua script example:
+--- write data to a file called test,
+--- data -> 'name:YourName:last_name:YourLastName' 
+local db = require("db")
+
+local write_record = db.write_record
+
+--- write_record returns two values, the key that the record was saved with 
+--- and the record itself, now we can use lua, to create for example, another data_to_add string,(see from line 16)
+--- in the format that the database expects, to write other files, 
+
+local key,rec = write_record('test','name:YourName:last_name:YourLastName')
+
+if rec == nil then
+	print('write record failed')
+else
+	print('key is '..key)
+
+	--- example: create a string for the dasbase,
+	--- this is usefull when we need to write some of the record data to the main db file (name_file)
+	--- the string st can be pass to another write_record().
+	
+	local st = string.format("n_code:%s:n_name:%s:ref:%d",rec.fields.name,rec.fields.last_name,key)
+	print(st)
+end
