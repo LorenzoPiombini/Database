97ef8c5990dfb2fbf64a8a83a1885f183a58b441
Lorenzo Piombini <piombinilorenzo1987@gmail.com>
2026-07-05 09:27:31 -0400

    fix(): error messages

--- network_interface/src/lua_start.c
+++ network_interface/src/lua_start.c
@@ -39,7 +39,7 @@ void check_config_file()
 	if(stat("/root/db/lua/db_config.lua",&file_data) == -1) {
 		return;
 	}
-	
+
 	if(sec == 0){
 		sec = file_data.st_mtim.tv_sec;
 		free_inactive_caches(cache);
@@ -68,36 +68,36 @@ int execute_lua_function(char *func_name, char *func_sig, ...)
 	lua_getglobal(L,func_name);
 
 	for(narg = 0; *func_sig != '\0'; narg++,func_sig++){
-	
+
 		luaL_checkstack(L,1,"too many arguments");
 
 		switch(*func_sig){
-		case 'r': /* Record_f */	
-			if(port_record(L,va_arg(vl,struct Record_f*)) == -1){
+			case 'r': /* Record_f */	
+				if(port_record(L,va_arg(vl,struct Record_f*)) == -1){
+					return -1;
+				}
+				break;
+			case 'd': /* double */	
+				lua_pushnumber(L,va_arg(vl,double));
+				break;
+			case 'i': /* integer*/	
+				lua_pushinteger(L,va_arg(vl,int));
+				break;
+			case 'I': /*unsigned integer*/	
+				lua_pushinteger(L,va_arg(vl,uint32_t));
+				break;
+			case 'l': /* long integer*/	
+				lua_pushinteger(L,va_arg(vl,long));
+				break;
+			case 's': /* string*/	
+				char *s = va_arg(vl,char*);
+				lua_pushstring(L,s);
+				break;
+			case '>': /*end of input*/
+				func_sig++;
+				goto fcall;
+			default:
 				return -1;
-			}
-			break;
-		case 'd': /* double */	
-			lua_pushnumber(L,va_arg(vl,double));
-			break;
-		case 'i': /* integer*/	
-			lua_pushinteger(L,va_arg(vl,int));
-			break;
-		case 'I': /*unsigned integer*/	
-			lua_pushinteger(L,va_arg(vl,uint32_t));
-			break;
-		case 'l': /* long integer*/	
-			lua_pushinteger(L,va_arg(vl,long));
-			break;
-		case 's': /* string*/	
-			char *s = va_arg(vl,char*);
-			lua_pushstring(L,s);
-			break;
-		case '>': /*end of input*/
-			func_sig++;
-			goto fcall;
-		default:
-			return -1;
 		}
 	}
 
@@ -116,61 +116,61 @@ fcall:
 		nres = -nres;
 		while(*func_sig){
 			switch(*func_sig){
-			case 'r':
-			{
-				/*record*/
-				if(port_table_to_record(L,*va_arg(vl,struct Record_f**)) == -1){
+				case 'r':
+					{
+						/*record*/
+						if(port_table_to_record(L,*va_arg(vl,struct Record_f**)) == -1){
+							clear_lua_stack();
+							return -1;
+						}
+						break;
+					}
+				case 'd':
+					{
+						int is_num;
+						double d = lua_tonumberx(L,nres,&is_num);
+						if(!is_num){
+							clear_lua_stack();
+							return -1;
+						}
+						*va_arg(vl, double *) = d;
+						break;
+					}
+				case 'i':
+					{
+						int is_num;
+						int l = (int)lua_tointegerx(L,nres,&is_num);
+						if(!is_num || l < 0){
+							clear_lua_stack();
+							return -1;
+						}
+						*va_arg(vl, int*) = l;
+						break;
+					}
+				case 'l':
+					{
+						int is_num;
+						long long l = (long long)lua_tointegerx(L,nres,&is_num);
+						if(!is_num || l < 0){
+							clear_lua_stack();
+							return -1;
+						}
+						*va_arg(vl, long long*) = l;
+						break;
+					}
+				case 's':
+					{
+						char *s = (char*)lua_tostring(L,nres);
+						if(!s){
+							clear_lua_stack();
+							return -1;
+						}
+						*va_arg(vl,char **) = s;
+						break;
+					}
+				default:
 					clear_lua_stack();
 					return -1;
-				}
-				break;
-			}
-			case 'd':
-			{
-				int is_num;
-				double d = lua_tonumberx(L,nres,&is_num);
-				if(!is_num){
-					clear_lua_stack();
-					return -1;
-				}
-				*va_arg(vl, double *) = d;
-				break;
-			}
-			case 'i':
-			{
-				int is_num;
-				int l = (int)lua_tointegerx(L,nres,&is_num);
-				if(!is_num || l < 0){
-					clear_lua_stack();
-					return -1;
-				}
-				*va_arg(vl, int*) = l;
-				break;
-			}
-			case 'l':
-			{
-				int is_num;
-				long long l = (long long)lua_tointegerx(L,nres,&is_num);
-				if(!is_num || l < 0){
-					clear_lua_stack();
-					return -1;
-				}
-				*va_arg(vl, long long*) = l;
-				break;
-			}
-			case 's':
-			{
-				char *s = (char*)lua_tostring(L,nres);
-				if(!s){
-					clear_lua_stack();
-					return -1;
-				}
-				*va_arg(vl,char **) = s;
-				break;
-			}
-			default:
-				clear_lua_stack();
-				return -1;
 			}
 			nres++;
 			func_sig++;
@@ -199,7 +199,7 @@ int get_function_signature(char *function_name,char *signature)
 		lua_pop(L,1);
 		return -1;
 	}
-	
+
 	lua_pop(L,1);
 	return 0;
 }
