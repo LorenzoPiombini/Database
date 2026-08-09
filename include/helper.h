e9a942cb670e28fb486f5df01a60a067181f4265
Lorenzo Piombini <piombinilorenzo1987@gmail.com>
2026-08-09 15:38:59 -0400

    add(): Windows code

--- include/helper.h
+++ include/helper.h
@@ -7,5 +7,11 @@
 unsigned char create_empty_file(int fd_schema, int fd_index, int bucket_ht);
 unsigned char append_to_file(int *fds, char *file_path, char *key,
 		char files[][MAX_FILE_PATH_LENGTH],char *data_to_add, HashTable *ht);
+
+#if defined(_WIN32)
+int create_file_with_schema(HANDLE fd_schema, HANDLE fd_index, char *schema_def, int bucket_ht, int indexes, int file_field);
+#else
 int create_file_with_schema(int fd_schema,  int fd_index, char *schema_def, int bucket_ht, int indexes, int file_field);
+#endif
+
 #endif
